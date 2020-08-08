cfa3c18bb87a82e2d7ef9153a2ea0fdd8078de13
James P Bondeson <jpbond58@gmail.com>
2020-08-08 02:43:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,16 +1,8 @@
 # Write your methods here
-def loop_message_five_times message
-  message == "Hello World"
-puts message
-puts message
-puts message
-puts message
-puts message
-end
-
-
-message == "Hello Moon."
-  while message[number] do
-    number == 5
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
     puts message
+    count += 1
   end
+end
